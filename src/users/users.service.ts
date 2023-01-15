c0c84971e565974187b929b341be82d45b88694b
Mehmet Can <mehmetcanozkulekci@gmail.com>
2023-01-16 00:24:10 +0300

    Controller and service for user is set for DI. Create function is added to the user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { create } from 'domain';
+import { stringify } from 'querystring';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  create(email: string, password: string) {
+    // Creating an instance of an entity, does not update the db
+    const user = this.repo.create({ email, password });
+    // To update the db
+    return this.repo.save(user);
+  }
 }
