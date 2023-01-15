de42bb26afdd7390cec7a86b300ab790c4906af2
Mehmet Can <mehmetcanozkulekci@gmail.com>
2023-01-16 01:43:51 +0300

    removeUser controller added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Post,
   Patch,
   Query,
+  Delete,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -26,5 +27,10 @@ export class UsersController {
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
